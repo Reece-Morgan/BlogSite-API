03df879ebc76c62ce143cddc4b66477930a401f7
Reece <reecemorgandev@gmail.com>
2024-07-01 10:10:27 +0100

    added item and context models

---
+++ Models/BlogContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogSite_API.Models
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options) { }
+
+        public DbSet<BlogItem> BlogItems { get; set; }
+    }
+}
---
+++ Models/BlogItem.cs
@@ -0,0 +1,16 @@
+﻿namespace BlogSite_API.Models
+{
+    public class BlogItem
+    {
+        public int Id {  get; set; }
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public int ReadLength { get; set; }
+    }
+}
