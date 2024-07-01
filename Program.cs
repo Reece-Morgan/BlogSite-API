df10f8e3abbb1a18052f9875f20938f32d9737b0
Reece <reecemorgandev@gmail.com>
2024-07-01 10:10:40 +0100

    added db context

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
+using BlogSite_API.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<BlogContext>(opt => opt.UseInMemoryDatabase("BlogList"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
